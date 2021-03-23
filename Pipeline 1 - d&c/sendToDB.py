0d14faac99666b8c1afeba2b85db8f191d187a51
gcovillo <40395875+gcovillo@users.noreply.github.com>
2021-03-23 09:02:00 -0400

    Add files via upload

---
+++ Pipeline 1 - d&c/sendToDB.py
@@ -0,0 +1,25 @@
+import mympsql
+from mario import setSchedule
+
+@setSchedule(df, CTRLM = 'send2db(df)', 
+             runAfter = None, maxAge = None, 
+             windowOpen = '[sun]', 
+             windowOpenTime = '4am', 
+             widowCloseIn = '1hr')
+def send2db(data):
+    connection = pymysql.connect(host='localhost',
+                                 user = 'sampleUser',
+                                 password = 'pass',
+                                 db = 'forecasts')
+    cursor = connection.cursor()
+
+    sql = "INSERT INTO 'forecasts' ('Date', 'Predicted Close') VALUES (%s, %s)"
+
+    for i in range(len(data)):
+        cursor.execute(sql, (data.at[i, 'Date'], data.at[i, 'close']))
+        connection.commit()
+    
+    connection.close()
+    
+    return True
+
