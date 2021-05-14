07f442289542058bbbeec6da401a67fdf3a3a953
Covillo, Gillian <gillian_covillo@bmc.com>
2021-05-14 01:15:09 -0400

    added fourth pipeline

---
+++ Pipeline 4 - chatbot/clean_entity_regex.py
@@ -0,0 +1,43 @@
+"""
+CTRL-M clean_entity_regex()
+Window-open mon,tue,wed,thur,fri,sat,sun 12:30am
+Window-close-in 1hr
+run-after clean_entity_dict.py
+run-at None
+"""
+
+import pandas as pd
+import os
+
+
+def clean_entity_regex():
+    regex = []
+
+    folder_path = "home/ubuntu/test/data/raw/v001/Entity Data/"
+    folder = os.listdir(folder_path)
+    for file in folder:
+        if 'regex' in file:
+            regex.append(file)
+
+    entity_names = []
+    regex_arrays = []
+
+    for file in regex:
+        entity_name = file.replace("EN_RB_ANY_ANY_", "").replace("_regex", "")
+        folder_path = "data/raw/v001/Entity Data/"
+        path = folder_path + file
+        entity_file = open(path, "r")
+        temp_array = []
+        for line in entity_file:
+            temp_array.append(line.replace("\n", ""))
+
+        entity_names.append(entity_name)
+        regex_arrays.append(temp_array)
+
+    regex_df = pd.DataFrame({'Entityname': entity_names,
+                             'regex_array': regex_arrays})
+
+    regex_df.to_csv('entityRegex.csv')
+
+
+clean_entity_regex()
---
+++ Pipeline 4 - chatbot/clean_small_talk.py
@@ -0,0 +1,38 @@
+"""
+CTRL-M clean_small_talk()
+Window-open mon,tue,wed,thur,fri,sat,sun 11:20pm
+Window-close-in 1hr
+run-after get_intent_data.py
+run-at None
+"""
+
+import pandas as pd
+import os
+
+
+def clean_small_talk():
+    path = "home/ubuntu/test/data/raw/v001/SmallTalk/smalltalk.txt"
+    small_talk = open(path, "r")
+    small_talk = small_talk.readlines()
+
+    user_response = []
+    bot_response = []
+    temp_bot_response = []
+
+    for line in small_talk:
+        if ("[" in line) and ("]" in line):
+            user_response.append(line.replace("[", "").replace("]", "").replace("\n", ""))
+        elif line.replace(" ", "") == "\n":
+            bot_response.append(list(temp_bot_response))
+            temp_bot_response.clear()
+        else:
+            temp_bot_response.append(line.replace("\n", ""))
+
+    small_talk_df = pd.DataFrame({'input': user_response,
+                                  'intent': 'small_talk',
+                                  'response_array': bot_response})
+
+    small_talk_df.to_csv('smallTalk.csv')
+
+
+clean_small_talk()
