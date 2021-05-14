4223c48af2bbffcb792a01f695a60f55cfaf3110
Covillo, Gillian <gillian.covillo@gmail.com>
2021-05-14 01:51:56 -0400

    so_close

--- Pipeline 4 - chatbot/clean_trained_entities.py
+++ Pipeline 4 - chatbot/clean_trained_entities.py
@@ -16,7 +16,7 @@ def clean_trained_entities():
     entity_names = []
     values = []
     os.chdir('..')
-    folder_path = "data/raw/v001/Entity Data"
+    folder_path = "data/raw/v001/Entity Data/"
     folder = os.listdir(folder_path)
     for file in folder:
         if 'samples' in file:
