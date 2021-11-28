870662e1cdcc26cc3e5e48f0e325efac123e0b2d
Covillo, Gillian <gillian.covillo@gmail.com>
2021-11-28 09:53:37 -0800

    adding first pipeline to mario system
    chatbot pipeline

---
+++ demo_chatbot/get_unique_intents.py
@@ -0,0 +1,35 @@
+"""
+function get_unique_intents()
+Window-open mon,tue,wed,thur,fri,sat,sun 12:30am
+Window-close-in 1hr
+run-after merge_intents.py
+"""
+
+import pandas as pd
+import os
+
+
+def get_unique_intents():
+    df = pd.read_csv('merged_intent_data.csv', low_memory = False, dtype={'use_case': 'string', 'intent': 'string',
+                                                                          'sentence': 'string', 'answer': 'string'})
+    words = []
+    word_dict = []
+
+    for i, sentence in enumerate(df['sentence']):
+        for word in sentence.split(" "):
+            use_case = df.at[i, 'use_case']
+            intent = df.at[i, 'intent']
+            pairing = {use_case: intent}
+            if word not in words:
+                words.append(word)
+                word_dict.append([pairing])
+            else:
+                ind = words.index(word)
+                need_to_update = word_dict[ind]
+                if pairing not in need_to_update:
+                    word_dict[ind].append(pairing)
+
+    unique_intents = pd.DataFrame({'Unique Word': words,
+                                   'Use_Case | Intent': word_dict})
+
+    unique_intents.to_csv('unique_intents.csv')
