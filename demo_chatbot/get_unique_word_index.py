a440c6efdc7e42c03999366232615e9944407c3f
Covillo, Gillian <gillian.covillo@gmail.com>
2022-01-03 09:50:53 -0800

    fix

--- demo_chatbot/get_unique_word_index.py
+++ demo_chatbot/get_unique_word_index.py
@@ -6,8 +6,7 @@ run-after get_unique_intents.py
 run-at 1am
 """
 
-'{"links":[{"id":"get_intent_data.py_8","source":"8","target":"9"},{"id":"clean_small_talk.py_9","source":"9","target":"5"},{"id":"clean_faq_files.py_5","source":"5","target":"1"},{"id":"clean_entity_dict.py_1","source":"1","target":"3"},{"id":"clean_entity_regex.py_3","source":"3","target":"2"},{"id":"clean_trained_entities.py_2","source":"2","target":"7"},{"id":"merge_intents.py_7","source":"7","target":"6"},{"id":"get_unique_intents.py_6","source":"6","target":"4"}],"nodes":[{"id":"8","label":"get_intent_data.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"9","label":"clean_small_talk.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"5","label":"clean_faq_files.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"1","label":"clean_entity_dict.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"3","label":"clean_entity_regex.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"2","label":"clean_trained_entities.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"7","label":"merge_intents.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"6","label":"get_unique_intents.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"},{"id":"4","label":"get_unique_word_index.py","status":"ColorStatus.Default","description":"Job Type: Service","icon":"app_connection"}]}'
-'import pandas as pd
+import pandas as pd
 
 def unique_word_intent_subintent_index():
     # read in data
