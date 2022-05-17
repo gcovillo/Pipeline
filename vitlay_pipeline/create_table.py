08f11c30633b5fc71f736f10ff87f7e7d2030de5
Covillo, Gillian <gillian_covillo@bmc.com>
2022-05-17 16:47:54 -0700

    test

--- vitlay_pipeline/create_table.py
+++ vitlay_pipeline/create_table.py
@@ -2,10 +2,10 @@
 """
 function create_table()
 Window-open mon,tue,wed,thur,sun
-Window-close-in 1hr 
+Window-close-in 1hr
 group_name vitaly_test_group
 owner Vitaly Pankratov
-pipeline = table
+pipeline = table 
 """
 
 import pandas as pd
