2bf5554dcbfee1f3c8ab798ba67336bb8fb9fc2a
Covillo, Gillian <gillian_covillo@bmc.com>
2022-05-17 07:02:27 -0700

    new run with edited code

--- vitlay_pipeline/double_the_table.py
+++ vitlay_pipeline/double_the_table.py
@@ -2,13 +2,13 @@
 import pandas as pd
 from mario import set_schedule
 
-@set_schedule(function = "double_table()",
+@set_schedule(function = "double_table(3)",
               runAfter = 'elongate_the_table.py', maxAge = '1hr',
               windowOpen = "mon,tue,wed,thur,sun",
               windowOpenTime = '12:30am',
               runAt = '4pm',
               windowCloseIn = '30min')
-def double_table():
+def double_table(y):
     df = pd.read_csv('table.csv')
-    df = df*2
+    df = df*y
     df.to_csv('table.csv', index=False)
