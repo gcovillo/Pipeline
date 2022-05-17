8a17b839ee2939c2574efeb5645591fb3cb97d52
Covillo, Gillian <gillian_covillo@bmc.com>
2022-05-17 09:20:31 -0700

    made change

--- vitlay_pipeline/double_the_table.py
+++ vitlay_pipeline/double_the_table.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from mario import set_schedule
 
-@set_schedule(function = "double_table(3)",
+@set_schedule(function = "double_table(2)",
               runAfter = 'elongate_the_table.py', maxAge = '1hr',
               windowOpen = "mon,tue,wed,thur,sun",
               windowOpenTime = '12:30am',
