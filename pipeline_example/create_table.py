01adc0706603c36d502466c6f032d6e55b530649
Stayermax <vitalkrat@gmail.com>
2022-05-19 12:30:28 +0300

    first test

---
+++ pipeline_example/create_table.py
@@ -0,0 +1,18 @@
+# First file
+"""
+function create_table()
+Window-open mon,tue,wed,thur,sun
+Window-close-in 1hr
+group_name vitaly_test_group
+owner Vitaly Pankratov
+pipeline = 'vitaly_test_1_19_05_2022'
+"""
+import pandas as pd
+
+
+def create_table():
+    data = [[1,2],[3,4], [5,6], [7,8]]
+    columns = ['A', 'B']
+
+    df = pd.DataFrame(data, columns=columns)
+    df.to_csv('table_1.csv', index=False)
