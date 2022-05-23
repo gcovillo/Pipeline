7e13fb89ad3059549fab289131a38ea76a1d8a03
Covillo, Gillian <gillian_covillo@bmc.com>
2022-05-23 15:45:07 -0700

    test2

--- pipeline_example/create_table.py
+++ pipeline_example/create_table.py
@@ -9,9 +9,11 @@ pipeline = 'vitaly_test_1_19_05_2022'
 """
 import pandas as pd
 
+
 def create_table():
     data = [[1,2],[3,4], [5,6], [7,8]]
     columns = ['A', 'B']
 
     df = pd.DataFrame(data, columns=columns)
     df.to_csv('table_1.csv', index=False)
+    df.to_csv('table2.csv')
